de167bbd6a53e50eb16af4a24597f5ee5f5f86f3
Rania10970 <66833518+Rania10970@users.noreply.github.com>
2021-06-10 16:49:47 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-require(["DSx/Helpers/API"], function (API) {
 
   widget.body.innerHTML = `
     <div style="float: left;width: 30vw">
@@ -12,15 +11,3 @@ require(["DSx/Helpers/API"], function (API) {
     </div>
   `;
 
-  document.getElementById("submit").addEventListener("click", submit);
-
-  function submit() {
-    var controller = document.getElementById("controller").value;
-    var method = document.getElementById("method").value;
-    var postData = JSON.parse(document.getElementById("postData").value);
-
-    API.call(controller, method, postData, (result) => {
-      document.getElementById("result").value = JSON.stringify(result);
-    });
-  }
-});
