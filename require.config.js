669b2911b6f19032d058cb3283eb2a495d821536
Rania10970 <66833518+Rania10970@users.noreply.github.com>
2021-06-10 17:43:08 +0200

    Update require.config.js

--- require.config.js
+++ require.config.js
@@ -2,7 +2,7 @@ define([], function () {
     'use strict';
 
     var SRC = 'https://raw.githubusercontent.com/Rania10970/Widget/main';
-    var LIBS = 'https://widgetrepository-thirdparty-cdn.3ds.com';
+    var LIBS = 'https://github.com/Rania10970?tab=repositories';
     var RES = 'https://widgetfactorydev.extranet.3ds.com/resources';
 
     require.config({
