caecb52d4d4b0891609f17b30725f231181d1ac3
Rania10970 <66833518+Rania10970@users.noreply.github.com>
2021-06-10 17:42:06 +0200

    Update require.config.js

--- require.config.js
+++ require.config.js
@@ -2,16 +2,18 @@ define([], function () {
     'use strict';
 
     var SRC = 'https://raw.githubusercontent.com/Rania10970/Widget/main';
-   
+    var LIBS = 'https://widgetrepository-thirdparty-cdn.3ds.com';
+    var RES = 'https://widgetfactorydev.extranet.3ds.com/resources';
 
     require.config({
         paths: {
             build: `${SRC}/main`,
-            
+            DSx: `${RES}/javascript/DSx`,
+            jquery: `${LIBS}/jquery-2.1.1/jquery`,
         },
     });
 
-    define('appDependencies');
+    define('appDependencies', ['jquery']);
 
     require(['appDependencies', 'build']);
 });
