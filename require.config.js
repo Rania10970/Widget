88c8ce791b495663bb6809236174a86125addcbf
Rania10970 <66833518+Rania10970@users.noreply.github.com>
2021-06-11 15:07:42 +0200

    Update require.config.js

--- require.config.js
+++ require.config.js
@@ -2,13 +2,12 @@ define([], function () {
     'use strict';
 
     var SRC = 'https://raw.githubusercontent.com/Rania10970/Widget/main';
-    var LIBS = 'https://github.com/Rania10970?tab=repositories';
-    var RES = 'https://github.com/Rania10970/Widget';
+  
 
     require.config({
         paths: {
             build: `${SRC}/main`,
-            DSx: `${RES}/javascript/DSx`,
+  
             jquery: `${LIBS}/jquery-2.1.1/jquery`,
         },
     });
